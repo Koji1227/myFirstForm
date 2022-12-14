41fa83350ccb66061895504cbff9ac5d659f156b
Koji1227 <115434297+Koji1227@users.noreply.github.com>
2022-12-14 14:59:03 +0900

    Third Commit

--- simple_form.py
+++ simple_form.py
@@ -11,7 +11,7 @@ def processPost():
     x = int(request.form['x'])
     y = int(request.form['y'])
     sum = x + y
-    return render_template('result.html', result = f"答えは{sum}です。")
+    return render_template('result.html', result = f"{x}足す{y}の答えは{sum}です。")
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8000, debug = True)
